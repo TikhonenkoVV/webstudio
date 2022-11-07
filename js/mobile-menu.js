ffd33f0bfa2b0d6508ab37ca4647d232d43231a6
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2022-11-07 17:37:45 +0200

    final fix

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,21 +1,24 @@
 (() => {
-    const refs = {
-        openMobileMenulBtn: document.querySelector('[mobile-menu-open]'),
-        closeMobileMenulBtn: document.querySelector('[mobile-menu-close]'),
-        mobileMenu: document.querySelector('[mobile-menu]'),
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const openMenuBtn = document.querySelector('.header__mobile-menu--open');
+    const closeMenuBtn = document.querySelector('.mobile-menu__btn-close');
+
+    const toggleMenu = () => {
+        const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+        openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+        mobileMenu.classList.toggle('is-open');
+
+        const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
+        bodyScrollLock[scrollLockMethod](document.body);
     };
 
-    refs.openMobileMenulBtn.addEventListener('click', toggleMobile);
-    refs.closeMobileMenulBtn.addEventListener('click', toggleMobile);
+    openMenuBtn.addEventListener('click', toggleMenu);
+    closeMenuBtn.addEventListener('click', toggleMenu);
 
-    function toggleMobile() {
-        refs.mobileMenu.classList.toggle('is-open');
-        refs.closeMobileMenulBtn.classList.toggle('is-open');
-        document.body.classList.toggle('modal-open');
-    }
-
-    window.matchMedia('(max-width: 767px)').addEventListener('change', e => {
+    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
         if (!e.matches) return;
-        refs.mobileMenu.classList.remove('is-open');
+        mobileMenu.classList.remove('is-open');
+        openMenuBtn.setAttribute('aria-expanded', false);
+        bodyScrollLock.enableBodyScroll(document.body);
     });
 })();
